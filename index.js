0abe2e090c194c3762a8ed47bce2a0595851d978
turpp <white.tripp@icloud.com>
2021-01-19 15:54:57 -0600

    commit tree 5a89126d47e2aa049224ee81a4693c073c29a3cd

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(array, fn){
+   let a =[]
+   for(let i = 0; i < array.length; i++){
+       a.push(fn(array[i]))
+   }
+   return a
+}
+
+function reduce(array, fn, start = 0){
+    let base = start
+    let a = []
+    for(let i = 0; i < array.length; i++){
+    //    base = fn(array[i], base)
+        if (array[i]== false){
+            return false
+        } else {
+            base = fn(array[i], base)
+        }
+    }
+    
+    return base
+}
